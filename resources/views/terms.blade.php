61192d45663f671dc9054b356ecb79bd1b40521d
Darlinton <darlinton@gmail.com>
2024-01-10 14:56:38 -0300

    tutorial#30

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,9 +1,21 @@
 @extends('layout.layout')
 
 @section('content')
-
-<h1>Terms</h1>
-"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
-
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-side-bar')
+        </div>
+        <div class="col-6">
+            <h1>Terms</h1>
+            <div>
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is
+                pain..."
+            </div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
 @endsection
